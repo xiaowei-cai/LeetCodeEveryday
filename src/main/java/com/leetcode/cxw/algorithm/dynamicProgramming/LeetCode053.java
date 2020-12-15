1d9d2e1ca5e48de62f0500a46ec8951d2e372dca
caixiaowei <caixiaowei@bixin.cn>
2020-12-15 18:04:46 +0800

    最大子序和

--- src/main/java/com/leetcode/cxw/algorithm/dynamicProgramming/LeetCode053.java
+++ src/main/java/com/leetcode/cxw/algorithm/dynamicProgramming/LeetCode053.java
@@ -1,9 +1,37 @@
 package com.leetcode.cxw.algorithm.dynamicProgramming;
 
 /**
- * @author caixiaowei
- * @date 2020/12/15 17:45
+ * 【最大子序和】
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 进阶:
+ *
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 public class LeetCode053 {
+    public static int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {
+                -2, 10, -3, 4, -1, 2, 1, -5, 4
+        };
+        System.out.println(maxSubArray(nums));
+    }
 }
