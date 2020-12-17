81199643dff5a6907edea0f30674b785615a8358
caixiaowei <caixiaowei@bixin.cn>
2020-12-17 15:36:56 +0800

    两数相加

---
+++ src/main/java/com/leetcode/cxw/algorithm/linkedList/LeetCode002.java
@@ -0,0 +1,51 @@
+package com.leetcode.cxw.algorithm.linkedList;
+
+import com.leetcode.cxw.algorithm.common.ListNode;
+
+/**
+ * 【两数相加】
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class LeetCode002 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        //进位
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (carry == 1) {
+            tail.next = new ListNode(carry);
+        }
+
+        return head;
+    }
+}
