4c0c73e57928a09d9fd5609ef889de8b91fde5d5
xiaowei_cai <caixiaowei910325@163.com>
2020-12-28 11:00:05 +0800

    Update LRUCache.java

--- src/main/java/com/leetcode/cxw/algorithm/linkedList/LRUCache.java
+++ src/main/java/com/leetcode/cxw/algorithm/linkedList/LRUCache.java
@@ -12,14 +12,14 @@ import java.util.Map;
  * void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
  *  
  * 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
- * <p>
+ * 
  * 示例：
  * 输入
  * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
  * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
  * 输出
  * [null, null, null, 1, null, -1, null, -1, 3, 4]
- * <p>
+ * 
  * 解释
  * LRUCache lRUCache = new LRUCache(2);
  * lRUCache.put(1, 1); // 缓存是 {1=1}
@@ -37,7 +37,7 @@ import java.util.Map;
  * 0 <= key <= 3000
  * 0 <= value <= 104
  * 最多调用 3 * 104 次 get 和 put
- * <p>
+ * 
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/lru-cache
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
