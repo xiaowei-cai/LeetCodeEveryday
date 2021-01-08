6e68846dc14222521c2116d054b0f703a10a13fb
caixiaowei <caixiaowei910325@163.com>
2021-01-08 10:49:10 +0800

    整数的各位积和之差

---
+++ src/main/java/com/leetcode/cxw/algorithm/mathematics/LeetCode1281.java
@@ -0,0 +1,40 @@
+package com.leetcode.cxw.algorithm.mathematics;
+
+/**
+ * 【整数的各位积和之差】
+ * 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+ *
+ * 示例 1：
+ * 输入：n = 234
+ * 输出：15
+ * 解释：
+ * 各位数之积 = 2 * 3 * 4 = 24
+ * 各位数之和 = 2 + 3 + 4 = 9
+ * 结果 = 24 - 9 = 15
+ *
+ * 示例 2：
+ * 输入：n = 4421
+ * 输出：21
+ * 解释：
+ * 各位数之积 = 4 * 4 * 2 * 1 = 32
+ * 各位数之和 = 4 + 4 + 2 + 1 = 11
+ * 结果 = 32 - 11 = 21
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class LeetCode1281 {
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int sub = 1;
+        String number = String.valueOf(n);
+        for (int i = 0; i < number.length(); i++) {
+            int str = Integer.parseInt(String.valueOf(number.charAt(i)));
+            sum += str;
+            sub *= str;
+        }
+        return sub - sum;
+    }
+}
