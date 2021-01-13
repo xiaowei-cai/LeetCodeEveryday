af3c00117ee2ca46bcedc26903958b2db9b1895d
caixiaowei <caixiaowei910325@163.com>
2021-01-13 10:45:54 +0800

    接雨水

---
+++ src/main/java/com/leetcode/cxw/algorithm/array/LeetCode042.java
@@ -0,0 +1,90 @@
+package com.leetcode.cxw.algorithm.array;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 【接雨水】
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * 示例 1：
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ *
+ * 示例 2：
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/trapping-rain-water
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class LeetCode042 {
+    //暴力 O(n²)
+    public int trap(int[] height) {
+        int ans = 0;
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int max_left = 0, max_right = 0;
+            for (int j = i; j >= 0; j--) {
+                max_left = Math.max(max_left, height[j]);
+            }
+            for (int j = i; j < size; j++) {
+                max_right = Math.max(max_right, height[j]);
+            }
+            ans += Math.min(max_left, max_right) - height[i];
+        }
+        return ans;
+    }
+
+    //栈 O(n)
+    public int trap1(int[] height) {
+        int ans = 0, current = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        while (current < height.length) {
+            while (!stack.isEmpty() && height[current] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int distance = current - stack.peek() - 1;
+                int bounded_height = Math.min(height[current], height[stack.peek()]) - height[top];
+                ans += distance * bounded_height;
+            }
+            stack.push(current++);
+        }
+        return ans;
+    }
+
+    //双指针 O(n)
+    public int trap2(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    ans += (left_max - height[left]);
+                }
+                ++left;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    ans += (right_max - height[right]);
+                }
+                --right;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(new LeetCode042().trap(height));
+    }
+}
