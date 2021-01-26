81958098beb7fcdee28e258d85ee837cde7e2391
caixiaowei <caixiaowei910325@163.com>
2021-01-26 19:17:39 +0800

    反转字符串中的单词III

--- src/main/java/com/leetcode/cxw/algorithm/strings/LeetCode557.java
+++ src/main/java/com/leetcode/cxw/algorithm/strings/LeetCode557.java
@@ -1,7 +1,7 @@
 package com.leetcode.cxw.algorithm.strings;
 
 /**
- * 【反转字符串中的单词 III】
+ * 【反转字符串中的单词III】
  * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
  * 示例：
  * 输入："Let's take LeetCode contest"
@@ -14,6 +14,27 @@ package com.leetcode.cxw.algorithm.strings;
 
 public class LeetCode557 {
     public String reverseWords(String s) {
-        return "";
+        int n = s.length();
+        int start = 0;
+        int end = 0;
+        StringBuilder sb = new StringBuilder(s);
+        while (start < n) {
+            while (end < n && s.charAt(end) != ' ') {
+                end++;
+            }
+            reverse(sb, start, end - 1);
+            start = end + 1;
+            ++end;
+        }
+        return sb.toString();
+    }
+
+    //翻转字符串
+    private void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
+        }
     }
 }
