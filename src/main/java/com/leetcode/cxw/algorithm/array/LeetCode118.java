ae37b352f7f98b96c131bdb750440a02eca16a49
xw.cai <caixiaowei910325@163.com>
2021-03-21 22:40:21 +0800

    杨辉三角

---
+++ src/main/java/com/leetcode/cxw/algorithm/array/LeetCode118.java
@@ -0,0 +1,40 @@
+package com.leetcode.cxw.algorithm.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 【杨辉三角】
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * 示例:
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+}
---
+++ src/main/java/com/leetcode/cxw/algorithm/array/LeetCode119.java
@@ -0,0 +1,37 @@
+package com.leetcode.cxw.algorithm.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 【杨辉三角II】
+ * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * 示例:
+ * 输入: 3
+ * 输出: [1,3,3,1]
+ * 进阶：
+ * 你可以优化你的算法到 O(k) 空间复杂度吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode119 {
+    public List<Integer> getRow(int rowIndex) {
+        //先生成杨辉三角的前n行，再根据列表索引取结果
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret.get(rowIndex);
+    }
+}
