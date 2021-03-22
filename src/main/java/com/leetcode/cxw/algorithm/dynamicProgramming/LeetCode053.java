199c1dd0a36dbb958e5e52f0d8f730adaa2acb3b
caixiaowei <caixiaowei910325@163.com>
2021-03-22 19:51:53 +0800

    update最大连续子序和

--- src/main/java/com/leetcode/cxw/algorithm/dynamicProgramming/LeetCode053.java
+++ src/main/java/com/leetcode/cxw/algorithm/dynamicProgramming/LeetCode053.java
@@ -28,10 +28,23 @@ public class LeetCode053 {
         return res;
     }
 
+
+    public static int maxSubArray1(int[] nums) {
+        //动态规划思想
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + Math.max(dp[i - 1], 0);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         int[] nums = {
                 -2, 10, -3, 4, -1, 2, 1, -5, 4
         };
-        System.out.println(maxSubArray(nums));
+        System.out.println(maxSubArray1(nums));
     }
 }
