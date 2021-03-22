79590d18ab118cdaddf6ebe176c19ac81797f325
蔡晓伟 <caixiaowei@bixin.cn>
2021-03-22 21:47:48 +0800

    update 001

--- src/main/java/com/leetcode/cxw/algorithm/array/LeetCode001.java
+++ src/main/java/com/leetcode/cxw/algorithm/array/LeetCode001.java
@@ -1,5 +1,8 @@
 package com.leetcode.cxw.algorithm.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 【两数之和】
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -8,7 +11,7 @@ package com.leetcode.cxw.algorithm.array;
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
+ * 
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -24,6 +27,18 @@ public class LeetCode001 {
                     result[1] = j;
                     return result;
                 }
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
             }
         }
         return new int[]{};
